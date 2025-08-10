5f00e6f594cfa15d1bec3fbc0937e3d7f3ef2637
Mohamed Sharaf <contact@sharaf.cloud>
2025-08-10 16:33:48 +0300

    Update test_client_script.py

--- frappe/custom/doctype/client_script/test_client_script.py
+++ frappe/custom/doctype/client_script/test_client_script.py
@@ -1,7 +1,9 @@
 # Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
 # License: MIT. See LICENSE
-from frappe.tests import IntegrationTestCase
+from frappe.tests.utils import FrappeTestCase
+
+# test_records = frappe.get_test_records('Client Script')
 
 
-class TestClientScript(IntegrationTestCase):
+class TestClientScript(FrappeTestCase):
 	pass
