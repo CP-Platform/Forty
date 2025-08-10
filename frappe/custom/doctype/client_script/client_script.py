e07ee86516a571c4c23f1e1817871b87fd0c315c
Mohamed Sharaf <contact@sharaf.cloud>
2025-08-10 16:32:27 +0300

    Update client_script.py

--- frappe/custom/doctype/client_script/client_script.py
+++ frappe/custom/doctype/client_script/client_script.py
@@ -13,13 +13,22 @@ class ClientScript(Document):
 	if TYPE_CHECKING:
 		from frappe.types import DF
 
+		client_script_analytic: DF.TextEditor | None
+		client_script_analytic_by_user: DF.TextEditor | None
+		documentation: DF.Attach | None
+		documentation_2: DF.Attach | None
+		documentation_3: DF.Attach | None
 		dt: DF.Link
 		enabled: DF.Check
 		module: DF.Link | None
+		notes: DF.SmallText | None
+		reference_app: DF.Link | None
 		script: DF.Code | None
+		script_code_name: DF.Data | None
+		version: DF.Data | None
+		version_log: DF.Data | None
 		view: DF.Literal["List", "Form"]
 	# end: auto-generated types
-
 	def on_update(self):
 		frappe.clear_cache(doctype=self.dt)
 
